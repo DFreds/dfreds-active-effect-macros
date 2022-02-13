bf83a54750b2c52f6fdec3d8cbd55064b7d998d8
Derek Fredrickson <dfreds08@gmail.com>
2022-02-13 01:33:29 -0500

    Add support for saving and executing macros

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 import ActiveEffectMacros from './active-effect-macros.js';
+import Constants from './constants.js';
 
 Hooks.once('init', () => {
   // new Settings().registerSettings();
@@ -13,6 +14,8 @@ Hooks.once('ready', () => {
 });
 
 Hooks.on('renderActiveEffectConfig', (activeEffectConfig, html, data) => {
+  const currentMacro =
+    activeEffectConfig?.object?.getMacro()?.data?.command ?? '';
   const activeEffectNav = html.find('nav');
   activeEffectNav.append(
     '<a class="item" data-tab="macro"><i class="fas fa-code"></i> Macro</a>'
@@ -23,35 +26,71 @@ Hooks.on('renderActiveEffectConfig', (activeEffectConfig, html, data) => {
   effectsTab.after(
     `
     <section class="tab" data-tab="macro">
-      <form class="editable flexcol" autocomplete="off">
-        <div class="form-group">
-          <label>Scope</label>
-            <select name="scope" disabled="">
-              <option value="global" selected="">global</option>
-              <option value="actors">actors</option>
-              <option value="actor">actor</option>
-            </select>
-        </div>
-
-        <div class="form-group">
-          <label>Type</label>
-          <select name="type" disabled="">
-            <option value="script" selected="">script</option>
-            <option value="chat">chat</option>
+      <div class="form-group">
+        <label>Scope</label>
+          <select name="scope" disabled="">
+            <option value="global" selected="">global</option>
+            <option value="actors">actors</option>
+            <option value="actor">actor</option>
           </select>
-        </div>
+      </div>
 
-        <div class="form-group stacked command">
-          <label>Command</label>
-          <textarea name="command"></textarea>
-        </div>
-      </form>
+      <div class="form-group">
+        <label>Type</label>
+        <select name="type" disabled="">
+          <option value="script" selected="">script</option>
+          <option value="chat">chat</option>
+        </select>
+      </div>
+
+      <div class="form-group stacked command">
+        <label>Command</label>
+        <textarea name="command">${currentMacro}</textarea>
+      </div>
     </section>
     `
   );
 
+  // TODO add way to execute
   // <footer class="sheet-footer flexrow">
-  //   <button type="submit"><i class="fas fa-save"></i> Save Macro</button>
-  //   <button class="execute" type="button"><i class="fas fa-dice-d20"></i> Execute Macro</button>
+  //   <button class="execute" type="button"><i class="fas fa-dice-d20"></i> Execute On Macro</button>
+  //   <button class="execute" type="button"><i class="fas fa-dice-d20"></i> Execute Off Macro</button>
   // </footer>
+});
+
+Hooks.on('preUpdateActiveEffect', (activeEffect, data, options, userId) => {
+  const command = data?.command;
+
+  if (!command) return;
+
+  activeEffect?.setMacro(
+    new Macro({
+      name: `${activeEffect.data.label} macro`,
+      type: CONST.MACRO_TYPES.SCRIPT,
+      author: game.user.id,
+      command,
+    })
+  );
+});
+
+Hooks.on('createActiveEffect', (activeEffect, data, userId) => {
+  if (activeEffect?.hasMacro()) {
+    activeEffect.executeMacro('on');
+  }
+});
+
+Hooks.on('deleteActiveEffect', (activeEffect, data, userId) => {
+  if (activeEffect?.hasMacro() && activeEffect?.data?.disabled === false) {
+    activeEffect.executeMacro('off');
+  }
+});
+
+Hooks.on('updateActiveEffect', (activeEffect, data, options, userId) => {
+  if (!activeEffect?.hasMacro()) return;
+
+  if (data?.disabled === true) {
+    activeEffect.executeMacro('off');
+  } else if (data?.disabled === false) {
+    activeEffect.executeMacro('on');
+  }
 });
